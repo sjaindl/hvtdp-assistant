1118a50dc3f4b0e92fe37f6f3c4e96b8024a8d78
Stefan Jaindl <jaindl.stefan@gmail.com>
2025-11-27 16:47:36 +0100

    feat: Add local docs loader

--- ingest/docs_reader.py
+++ ingest/docs_reader.py
@@ -1,12 +1,14 @@
-import os
-import pandas as pd
 from llama_index.core.schema import Document
 from llama_index.core import SimpleDirectoryReader
 from llama_index.readers.file import PDFReader
+import os
+import pandas as pd
 
-def load_local_docs(folder="docs"):
+from utils.summarizer import summarize_doc
+
+def load_local_docs(folder = "docs"):
     docs = []
-    docs += load_pdf(folder)
+    docs += load_pdfs(folder)
     for file in os.listdir(folder):
         if file.endswith(".csv"):
             docs += load_csv(os.path.join(folder, file))
@@ -14,15 +16,22 @@ def load_local_docs(folder="docs"):
 
 def load_csv(path):
     df = pd.read_csv(path)
-    text = df.to_string(index=False)
-    return [Document(text=text)]
+    text = df.to_string(index = False)
+    return [Document(text = text, metadata = {"source": path})]
 
-def load_pdf(path):
+def load_pdfs(path):
     parser = PDFReader()
     file_extractor = {".pdf": parser}
     reader = SimpleDirectoryReader(
-        path, file_extractor=file_extractor
+        path, file_extractor = file_extractor
     )
 
     data = reader.load_data()
-    return data
+
+    out = []
+    for d in data:
+        summary = summarize_doc(d)
+        meta = d.metadata
+        out.append(Document(text=summary, metadata=meta))
+
+    return out
